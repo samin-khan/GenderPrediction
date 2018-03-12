bec3a498c03baab2d2c399fecb3d713557bb54f6
Samin Khan <samin.khan995@gmail.com>
2018-03-11 23:03:17 -0400

    Updated with print runtime statements

--- sk/3-letter Scheme.py
+++ sk/3-letter Scheme.py
@@ -1,14 +1,8 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import LeaveOneOut
-from sklearn.linear_model import LogisticRegression
-import operator
-import matplotlib.pyplot as plt
-from openpyxl import load_workbook
 import time
 from string import ascii_lowercase
 import pickle
-from scipy.sparse import csr_matrix
 
 df = pd.read_excel("MergedData.xlsx")
 df = df.replace({'Male':0}, regex=True)
@@ -17,13 +11,18 @@ df = df.dropna()
 
 dfLetters = df[['Name', 'Gender']]
 
+t0 = time.time()
+
 for c1 in ascii_lowercase:
+    
+    print(c1)
+
     dfLetters.insert(len(dfLetters.columns), "Starts with: " + c1, 0)
     dfLetters.loc[(dfLetters['Name'].str.startswith(c1)), "Starts with: " + c1] = 1
     
     dfLetters.insert(len(dfLetters.columns), "Ends with: " + c1, 0)
     dfLetters.loc[(dfLetters['Name'].str.endswith(c1)), "Ends with: " + c1] = 1
-    
+
     for c2 in ascii_lowercase:
         dfLetters.insert(len(dfLetters.columns), "Starts with: " + c1 + c2, 0)
         dfLetters.loc[(dfLetters['Name'].str.startswith(c1 + c2)), "Starts with: " + c1 + c2] = 1
@@ -37,8 +36,11 @@ for c1 in ascii_lowercase:
 
             dfLetters.insert(len(dfLetters.columns), "Ends with: " + c1 + c2 + c3, 0)
             dfLetters.loc[(dfLetters['Name'].str.endswith(c1 + c2 + c3)), "Ends with: " + c1 + c2 + c3] = 1
+    print("Letter " + c1 + " took: " + str(time.time() - t_char1))
 
-csr_df = csr_matrix(dfLetters.drop(["Name"], axis=1))
+t1 = time.time()
+print("Total runtime: " + str(t1-t0))
+
 out = open("Merged3LetterScheme.pkl", "wb")
-pickle.dump(csr_df, out)
+pickle.dump(dfLetters, out)
 out.close()
